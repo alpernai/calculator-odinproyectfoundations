f2115315d511170f9f8e6e935efa743a1f4ac38c
alpernai <148302974+alpernai@users.noreply.github.com>
2024-10-16 15:44:08 -0600

    Add hover and active CSS behavior to buttons and onClick inline functions.

---
+++ script.js
@@ -0,0 +1,19 @@
+const display = document.getElementById('display');
+
+
+
+function clearDisplay(key){};
+
+function getNumber(num){
+    display += num;
+};
+
+function getOperator(op){};
+
+function setDecimal(dec){};
+
+function getSign(plusorminus){};
+
+function deleteLast(del){};
+
+function calculate(operation){};
